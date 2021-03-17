754a248ac2ac8f867314b72efd4f23193aa5d2b4
Guillermo Moreno <guillermo.morvazquez@alumnos.upm.es>
2021-03-17 14:26:06 +0100

    comboBox

--- include/rqt_sarus_test_plugin/rqt_sarus_test_plugin_widget.h
+++ include/rqt_sarus_test_plugin/rqt_sarus_test_plugin_widget.h
@@ -6,6 +6,7 @@
 #include <std_msgs/String.h>
 #include "geometry_msgs/TwistStamped.h"
 
+
 namespace Ui {
 class TestPluginWidget;
 }
@@ -24,19 +25,24 @@ private slots:
     void on_pushButton_clicked();
     void Update();
 
+
 private:
     Ui::TestPluginWidget *ui;
 
     ros::NodeHandle ros_node_handle;
     ros::Subscriber my_subscriber;
-    ros::Subscriber speed;
+    ros::Subscriber speedx;
+    ros::Subscriber speedy;
     ros::Publisher buttonPublisher;
     void ros_data_callback(const std_msgs::String::ConstPtr &msg);
-    void ros_speed_callback(const geometry_msgs::TwistStamped::ConstPtr &v);
+    void ros_speedx_callback(const geometry_msgs::TwistStamped::ConstPtr &vx);
+    void ros_speedy_callback(const geometry_msgs::TwistStamped::ConstPtr &vy);
 
     //Variables
     std::string sub_message = "";
     float dronSpeed_x;
+    float dronSpeed_y;
+    int num_Drones = 2;
 
     QTimer *timer_1s;
 
--- src/rqt_sarus_test_plugin/rqt_sarus_test_plugin_widget.cpp
+++ src/rqt_sarus_test_plugin/rqt_sarus_test_plugin_widget.cpp
@@ -4,7 +4,9 @@
 #include <std_msgs/String.h>
 #include <QTimer>
 #include <QTime>
+#include <QString>
 #include "geometry_msgs/TwistStamped.h"
+
 
 TestPluginWidget::TestPluginWidget(QWidget *parent) :
     QWidget(parent),
@@ -15,6 +17,12 @@ TestPluginWidget::TestPluginWidget(QWidget *parent) :
     timer_1s = new QTimer(this);
     QObject::connect(timer_1s, SIGNAL(timeout()), this, SLOT(Update()));
     timer_1s->start(500);
+
+    std::string dron;
+    for(int i=0; i<num_Drones; i++){
+        dron = std::to_string(i);
+        ui->comboBox->addItem(dron.c_str());
+    }
 }
 
 TestPluginWidget::~TestPluginWidget()
@@ -25,18 +33,32 @@ TestPluginWidget::~TestPluginWidget()
 }
 
 void TestPluginWidget::Update() {
+    std::string speed;
+
+    QString currentDron;
+    currentDron.sprintf("%d", ui->comboBox->currentIndex());
+    if(currentDron.toInt() == 1){
+        speed = std::to_string(dronSpeed_x);
+        ui->speedxText->setText(speed.c_str());
+    }
+    else {
+        speed = std::to_string(dronSpeed_y);
+        ui->speedyText->setText(speed.c_str());
+    }
+
     ui->subText->setText(sub_message.c_str());
-    std::string x_speed;
-    x_speed = std::to_string(dronSpeed_x);
-    ui->speedText->setText(x_speed.c_str());
 }
 
 void TestPluginWidget::ros_data_callback(const std_msgs::String::ConstPtr &msg){
     this->sub_message = msg->data.c_str();
 }
 
-void TestPluginWidget::ros_speed_callback(const geometry_msgs::TwistStamped::ConstPtr &v){
-    this->dronSpeed_x = v->twist.linear.x;
+void TestPluginWidget::ros_speedx_callback(const geometry_msgs::TwistStamped::ConstPtr &vx){
+    this->dronSpeed_x = vx->twist.linear.x;
+}
+
+void TestPluginWidget::ros_speedy_callback(const geometry_msgs::TwistStamped::ConstPtr &vy){
+    this->dronSpeed_y = vy->twist.linear.y;
 }
 
 void TestPluginWidget::init_ROS_Node()
@@ -45,7 +67,8 @@ void TestPluginWidget::init_ROS_Node()
     // Write initialization code here (publishers, subscribers)
     buttonPublisher = ros_node_handle.advertise<std_msgs::String>("my_data",1);
     my_subscriber = ros_node_handle.subscribe("my_data", 1, &TestPluginWidget::ros_data_callback, this);
-    speed = ros_node_handle.subscribe("/drone107/motion_reference/speed", 1, &TestPluginWidget::ros_speed_callback, this);
+    speedx = ros_node_handle.subscribe("/drone107/motion_reference/speed", 1, &TestPluginWidget::ros_speedx_callback, this);
+    speedy = ros_node_handle.subscribe("/drone107/motion_reference/speed", 1, &TestPluginWidget::ros_speedy_callback, this);
 }
 
 void TestPluginWidget::on_pushButton_clicked()
